57d538406a0adf98a3a82f624fc33dfa2e70448c
Gabe Alexis <167690779+gabe91354@users.noreply.github.com>
2024-10-24 13:58:42 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,13 @@
+<?php
+$host = 'localhost';
+$db_name = 'securoserv';
+$username = 'root'; // Default XAMPP username
+$password = ''; // Default XAMPP password (empty)
 
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
